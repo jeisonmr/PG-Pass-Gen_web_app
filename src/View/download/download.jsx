81cde5615144a272258ce48ad0445c49fac5b57b
jeisonmr <jeisonmzr@gmail.com>
2023-04-15 11:30:22 -0500

    Vista tablet

--- src/View/download/download.jsx
+++ src/View/download/download.jsx
@@ -11,54 +11,6 @@ import './download.css';
 
 function Download() {
 
-  // Estilos del componente Donwload.
-  // const downView = {
-  //   width: "100%",
-  //   height: "140vh",
-  //   background: "var(--cl-2)",
-  //   display: "flex",
-  //   flexDirection: "column",
-  
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   position: "relative",
-  // };
-  // const svgTop = {
-  //   width: "100%",
-  //   position: "absolute",
-  //   top: "0",
-  // };
-  // const svgBottom = {
-  //   width: "100%",
-  //   position: "absolute",
-  //   bottom: "0",
-  // };
-  // const title_2 = {
-  //   fontFamily: "var(--font-1)",
-  //   fontSize: "50px",
-  //   textAlign: "center",
-  //   paddingTop: "70px",
-  //   color: "var(--cl-3)",
-  // };
-  // const subtitle = {
-  //   fontFamily: "var(--font-1)",
-  //   fontSize: "15px",
-  //   textAlign: "center",
-  //   padding: "40px 20px 10px 20px",
-  //   color: "var(--cl-3)",
-  // };
-  // const btnDown = {
-  //   width: "35px",
-  //   height: "35px",
-  //   margin: "30px 10px",
-  //   background: "var(--cl-3)",
-  //   borderRadius: "50%",
-  //   border: "none",
-  // };
-  // const imgIcon = {
-  //   width: "35px",
-  //   height: "35px",
-  // };
 
   return (
     <>
